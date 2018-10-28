265dea8f8e4d7c024bc299594d2c3ca2f3737da7
Au Yong Min Hao <mauyong@moneylion.com>
2018-10-28 16:27:06 +0800

    Introduce minimum password length validation

--- random-pass-gen.py
+++ random-pass-gen.py
@@ -5,17 +5,23 @@ import random
 def main():
     char = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
 
-    len = int(input('Password Length?:'))
-    num = int(input('How many to generate? (suggest more than 3):'))
+    try:
+        pwd_length = int(input('Password Length?: '))
+        if pwd_length < 5:
+            raise Exception("Please specify at least 5 for password length.")
 
+        num = int(input('How many to generate? (suggest more than 3): '))
 
-    for i in range(num):
-      pass_gen = ''
+        for i in range(num):
+            pass_gen = ''
 
-      for j in range(len):
-        pass_gen += random.choice(char)
-      
-      print (pass_gen)
+        for j in range(pwd_length):
+            pass_gen += random.choice(char)
+        
+        print (pass_gen)
+    
+    except Exception as e:
+        print("Error: {}".format(e))
   
   
 if  __name__ == '__main__':
